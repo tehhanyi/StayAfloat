0367dc23b1c393d2418f55409c73fc54dedcc396
Han Yi <70083662+tehhanyi@users.noreply.github.com>
2021-06-22 13:58:32 +0800

    Add files via upload

---
+++ style/navigation.js
@@ -0,0 +1,20 @@
+//HEADER & FOOTER JAVASCRIPT
+
+        function register() {
+            location.replace("http://localhost:8080/CSADMiniProject/register_page");
+        }
+        function navigateAim(){
+            location.replace("http://localhost:8080/CSADMiniProject/about_aim.php");
+        } 
+         function navigateTeam(){
+            location.replace("http://localhost:8080/CSADMiniProject/about_team.php");
+        }    
+        
+        function navigateAbout(){
+            location.replace("http://localhost:8080/CSADMiniProject/about_page.php");
+        } 
+        
+        function navigatePartner(){
+            location.replace("https://www.homage.sg/resources/active-ageing-singapore/");
+        }
+        
